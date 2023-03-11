d36f05bb844c20f958749de04deff3d63df64789
missou <695383649@qq.com>
2023-03-11 17:04:01 +0800

    fix eslint

--- lua/modules/cmp.lua
+++ lua/modules/cmp.lua
@@ -116,8 +116,8 @@ function M.setup()
   null_ls.setup({
     sources = {
       null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.eslint,
-      null_ls.builtins.diagnostics.eslint,
+      null_ls.builtins.formatting.eslint_d,
+      null_ls.builtins.diagnostics.eslint_d,
     },
   })
 
