c8273f380b71f85fcaae8a2cdcfa9969f8e9d6b7
missou <695383649@qq.com>
2023-03-17 20:01:21 +0800

    add dap

--- lua/modules/mason.lua
+++ lua/modules/mason.lua
@@ -2,13 +2,26 @@
 
 local M = {
   plugins = {
+    -- null-ls (linter)
+    'Carlosiano/null-ls.nvim',
+    -- dap
+    'mfussenegger/nvim-dap',
+    'rcarriga/nvim-dap-ui',
+    'jay-babu/mason-nvim-dap.nvim',
     -- mason
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
-    -- null-ls (linter)
-    'Carlosiano/null-ls.nvim',
     'jay-babu/mason-null-ls.nvim',
   }
+}
+
+M.nmap = {
+  {'<F5>', '<cmd>DapContinue<cr>'},
+  {'<F10>', '<cmd>DapTerminate<cr>'},
+  {'<F9>', '<cmd>DapToggleBreakpoint<cr>'},
+  {'<F6>', '<cmd>DapStepOver<cr>'},
+  {'<F7>', '<cmd>DapStepInto<cr>'},
+  {'<F8>', '<cmd>DapStepOut<cr>'},
 }
 
 function M.setup()
@@ -63,10 +76,72 @@ function M.setup()
   })
   require('mason-null-ls').setup_handlers {
     function(source_name, methods)
-      print(source_name, methods)
       require('mason-null-ls.automatic_setup')(source_name, methods)
     end
   }
+  local dap, dapui = require("dap"), require("dapui")
+  -- 
+  dapui.setup({
+    icons = {
+      expanded = "",
+      collapsed = "",
+      current_frame = ""
+    },
+    controls = {
+      icons = {
+        pause = "",
+        play = "",
+        step_into = "",
+        step_over = "",
+        step_out = "",
+        step_back = "",
+        run_last = "",
+        terminate = "",
+        disconnect = "",
+      }
+    }
+  })
+  dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+  end
+  dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+  end
+  dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+  end
+  local mason_dap = require('mason-nvim-dap')
+  mason_dap.setup({
+    automatic_setup = true,
+  })
+  mason_dap.setup_handlers({
+    function(source_name)
+      require('mason-nvim-dap.automatic_setup')(source_name)
+    end,
+  })
+  local dap_breakpoint = {
+    error = {
+      text = "",
+      texthl = "LspDiagnosticsSignError",
+      linehl = "",
+      numhl = "",
+    },
+    rejected = {
+      text = "",
+      texthl = "LspDiagnosticsSignHint",
+      linehl = "",
+      numhl = "",
+    },
+    stopped = {
+      text = "",
+      texthl = "LspDiagnosticsSignInformation",
+      linehl = "DiagnosticUnderlineInfo",
+      numhl = "LspDiagnosticsSignInformation",
+    },
+  }
+  vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
+  vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
+  vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
 end
 
 return M
