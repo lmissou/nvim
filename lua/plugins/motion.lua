eae2b425384db674751881e398286953a75f718c
missou <695383649@qq.com>
2024-07-13 14:12:46 +0800

    updata motion plugin

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -1,25 +1,17 @@
----------- 快速移动光标
+--------- 快速移动光标
 
 local M = {
   {
-    "ggandor/leap.nvim",
-    config = function()
-      require("leap").add_default_mappings()
-    end,
-  },
-  {
-    "ggandor/flit.nvim",
-    config = function()
-      require("flit").setup({
-        keys = { f = "f", F = "F", t = "t", T = "T" },
-        -- A string like "nv", "nvo", "o", etc.
-        labeled_modes = "v",
-        multiline = true,
-        -- Like `leap`s similar argument (call-specific overrides).
-        -- E.g.: opts = { equivalence_classes = {} }
-        opts = {},
-      })
-    end,
+    "folke/flash.nvim",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   },
 }
 
