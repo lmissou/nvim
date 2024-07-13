a5b37100b3692f0ddb2d4a742b55c519ee3b6df7
missou <695383649@qq.com>
2024-07-13 16:15:29 +0800

    update flutter lsp config

---
+++ lua/plugins/flutter.lua
@@ -0,0 +1,23 @@
+---------- flutter
+
+local M = {
+  "akinsho/flutter-tools.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim", -- optional for vim.ui.select
+  },
+  config = function()
+    -- Setup flutter(dart) lsp
+    local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local on_lsp_attach = require("plugins.lsp").on_lsp_attach
+    require("flutter-tools").setup({
+      lsp = {
+        capabilities = capabilities,
+        on_attach = on_lsp_attach,
+      },
+    })
+  end,
+}
+
+return M
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,29 @@
 ---------- lsp
 
+-- Use an on_attach function to only map the following keys
+function on_lsp_attach(_, bufnr)
+  local opts = { noremap = true, silent = true }
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  -- Mappings.
+  -- vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>Lspsaga preview_definition<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>Lspsaga signature_help<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lr", "<cmd>Lspsaga rename<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>la", "<cmd>Lspsaga code_action<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>le", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+end
+
 local M = {
   -- lsp config
   "neovim/nvim-lspconfig",
@@ -10,46 +34,12 @@ local M = {
     "onsails/lspkind-nvim",
     -- mason-lspconfig
     "williamboman/mason-lspconfig.nvim",
-    -- flutter
-    {
-      "akinsho/flutter-tools.nvim",
-      lazy = false,
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "stevearc/dressing.nvim", -- optional for vim.ui.select
-      },
-    },
   },
   config = function()
     -- lsp saga
     require("lspsaga").init_lsp_saga()
     -- lsp config
     local mason_lspconfig = require("mason-lspconfig")
-    -- Use an on_attach function to only map the following keys
-    -- after the language server attaches to the current buffer
-    local on_attach = function(_, bufnr)
-      local opts = { noremap = true, silent = true }
-      -- Enable completion triggered by <c-x><c-o>
-      vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-      -- Mappings.
-      -- vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-      -- See `:help vim.lsp.*` for documentation on any of the below functions
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>Lspsaga preview_definition<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>Lspsaga signature_help<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lr", "<cmd>Lspsaga rename<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>la", "<cmd>Lspsaga code_action<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>le", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
-    end
     -- Setup mason-lspconfig.
     mason_lspconfig.setup({})
     mason_lspconfig.setup_handlers({
@@ -57,19 +47,13 @@ local M = {
         local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
         require("lspconfig")[server_name].setup({
           capabilities = capabilities,
-          on_attach = on_attach,
+          on_attach = on_lsp_attach,
         })
       end,
     })
-    -- Setup flutter(dart) lsp
-    local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-    require("flutter-tools").setup({
-      lsp = {
-        capabilities = capabilities,
-        on_attach = on_attach,
-      },
-    })
   end,
+  -- after the language server attaches to the current buffer
+  on_lsp_attach = on_lsp_attach,
 }
 
 return M
