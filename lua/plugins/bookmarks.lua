2f784cc76205651393e456104db84b211af0bf21
missou <695383649@qq.com>
2024-07-20 16:32:32 +0800

    add bookmarks.nvim

---
+++ lua/plugins/bookmarks.lua
@@ -0,0 +1,86 @@
+---------- which-key
+
+local M = {
+  "tomasky/bookmarks.nvim",
+  keys = {
+    { "<leader>m", desc = "Bookmark" },
+    -- add or remove bookmark at current line
+    {
+      "<leader>mm",
+      mode = "n",
+      function()
+        require("bookmarks").bookmark_toggle()
+      end,
+      desc = "Bookmark Toggle",
+    },
+    -- add or edit mark annotation at current line
+    {
+      "<leader>mi",
+      mode = "n",
+      function()
+        require("bookmarks").bookmark_ann()
+      end,
+      desc = "Bookmark Ann",
+    },
+    -- clean all marks in local buffer
+    {
+      "<leader>mc",
+      mode = "n",
+      function()
+        require("bookmarks").bookmark_clean()
+      end,
+      desc = "Bookmark Clean",
+    },
+    -- jump to next mark in local buffer
+    {
+      "<leader>mn",
+      mode = "n",
+      function()
+        require("bookmarks").bookmark_next()
+      end,
+      desc = "Bookmark Next",
+    },
+    -- jump to previous mark in local buffer
+    {
+      "<leader>mp",
+      mode = "n",
+      function()
+        require("bookmarks").bookmark_prev()
+      end,
+      desc = "Bookmark Prev",
+    },
+    -- removes all bookmarks
+    {
+      "<leader>mx",
+      mode = "n",
+      function()
+        require("bookmarks").bookmark_clear_all()
+      end,
+      desc = "Bookmark Clear All",
+    },
+    -- show marked file list
+    {
+      "<leader>ml",
+      mode = "n",
+      function()
+        require("telescope").extensions.bookmarks.list()
+      end,
+      desc = "Bookmark List",
+    },
+  },
+  config = function()
+    require("bookmarks").setup({
+      -- sign_priority = 8,  --set bookmark sign priority to cover other sign
+      save_file = vim.fn.stdpath("data") .. "/.bookmarks", -- bookmarks save file path
+      keywords = {
+        ["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
+        ["@w"] = "❗", -- mark annotation startswith @w ,signs this icon as `Warn`
+        ["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
+        ["@n"] = "📓", -- mark annotation startswith @n ,signs this icon as `Note`
+      },
+    })
+    require("telescope").load_extension("bookmarks")
+  end,
+}
+
+return M
--- lua/plugins/fuzzyfinder.lua
+++ lua/plugins/fuzzyfinder.lua
@@ -4,33 +4,42 @@ local M = {
   {
     -- fuzzy finder
     "nvim-telescope/telescope.nvim",
+    keys = {
+      { "b", desc = "Buffer" },
+      { "bl", mode = "n", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+      { "f", desc = "File" },
+      { "ff", mode = "n", "<cmd>Telescope find_files<cr>", desc = "Find File" },
+      { "fp", mode = "n", "<cmd>Telescope projects<cr>", desc = "Find Project" },
+      { "fr", mode = "n", "<cmd>Telescope oldfiles<cr>", desc = "Open Recent File" },
+      { "fs", mode = "n", "<cmd>Telescope live_grep<cr>", desc = "Search string" },
+    },
     config = function()
       require("telescope").setup({
         extensions = {
           fzf = {
-            fuzzy = true,                   -- false will only do exact matching
+            fuzzy = true, -- false will only do exact matching
             override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true,    -- override the file sorter
-            case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
           },
         },
       })
-      local kb = require("config.keybindings")
-      kb.add_prefix("b", "Buffer")
-      kb.bind_leader("bl", "<cmd>Telescope buffers<cr>", "Buffers")
-      kb.add_prefix("f", "File")
-      kb.bind_leader("ff", "<cmd>Telescope find_files<cr>", "Find File")
-      kb.bind_leader("fp", "<cmd>Telescope projects<cr>", "Find Project")
-      kb.bind_leader("fr", "<cmd>Telescope oldfiles<cr>", "Open Recent File")
-      kb.bind_leader("fs", "<cmd>Telescope live_grep<cr>", "Search string")
+      -- local kb = require("config.keybindings")
+      -- kb.add_prefix("b", "Buffer")
+      -- kb.bind_leader("bl", "<cmd>Telescope buffers<cr>", "Buffers")
+      -- kb.add_prefix("f", "File")
+      -- kb.bind_leader("ff", "<cmd>Telescope find_files<cr>", "Find File")
+      -- kb.bind_leader("fp", "<cmd>Telescope projects<cr>", "Find Project")
+      -- kb.bind_leader("fr", "<cmd>Telescope oldfiles<cr>", "Open Recent File")
+      -- kb.bind_leader("fs", "<cmd>Telescope live_grep<cr>", "Search string")
     end,
   },
   {
     "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
     keys = {
-      { "<leader>f",          desc = "File" },
-      { "<leader>fb", mode = "n",   "<cmd>Telescope file_browser<cr>", desc = "File Browser" },
+      { "<leader>f", desc = "File" },
+      { "<leader>fb", mode = "n", "<cmd>Telescope file_browser<cr>", desc = "File Browser" },
     },
     config = function()
       require("telescope").load_extension("file_browser")
