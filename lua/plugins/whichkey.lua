5493f65da202d51cd9a5337173b6bd3fea0a9732
missou <695383649@qq.com>
2024-07-27 17:37:19 +0800

    update which-key config

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,14 +2,21 @@
 
 local M = {
   'folke/which-key.nvim',
+  lazy = false,
   opts = {},
+  keys = {
+    {
+      '<leader>?',
+      mode = { 'n', 'v' },
+      function()
+        require('which-key').show({ global = false })
+      end,
+      desc = 'Buffer Local Keymaps',
+    },
+  },
   config = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
-    local kb = require('config.keybindings')
-    kb.bind_leader('?', function()
-      require('which-key').show({ global = false })
-    end, 'Buffer Local Keymaps')
   end,
 }
 
