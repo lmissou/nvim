44c0edc412358d18eb4cfffc5211a0bcf1d9641e
missou <695383649@qq.com>
2025-04-05 22:20:31 +0800

    update telescope fzf config

--- lua/plugins/fuzzyfinder.lua
+++ lua/plugins/fuzzyfinder.lua
@@ -4,14 +4,15 @@ local M = {
   {
     -- fuzzy finder
     'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     keys = {
-      { '<leader>b', desc = 'Buffer' },
-      { '<leader>bb', mode = { 'n', 'v' }, '<cmd>Telescope buffers<cr>', desc = 'Buffers' },
-      { '<leader>f', desc = 'File' },
+      { '<leader>b',  desc = 'Buffer' },
+      { '<leader>bb', mode = { 'n', 'v' }, '<cmd>Telescope buffers<cr>',    desc = 'Buffers' },
+      { '<leader>f',  desc = 'File' },
       { '<leader>ff', mode = { 'n', 'v' }, '<cmd>Telescope find_files<cr>', desc = 'Find File' },
-      { '<C-p>', mode = { 'n', 'v' }, '<cmd>Telescope find_files<cr>', desc = 'Find File' },
-      { '<leader>fr', mode = { 'n', 'v' }, '<cmd>Telescope oldfiles<cr>', desc = 'Open Recent File' },
-      { '<leader>fs', mode = { 'n', 'v' }, '<cmd>Telescope live_grep<cr>', desc = 'Search string' },
+      { '<C-p>',      mode = { 'n', 'v' }, '<cmd>Telescope find_files<cr>', desc = 'Find File' },
+      { '<leader>fr', mode = { 'n', 'v' }, '<cmd>Telescope oldfiles<cr>',   desc = 'Open Recent File' },
+      { '<leader>fs', mode = { 'n', 'v' }, '<cmd>Telescope live_grep<cr>',  desc = 'Search string' },
       {
         '<leader>fc',
         mode = { 'n', 'v' },
@@ -38,20 +39,23 @@ local M = {
       require('telescope').setup({
         extensions = {
           fzf = {
-            fuzzy = true, -- false will only do exact matching
+            fuzzy = true,                   -- false will only do exact matching
             override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+            override_file_sorter = true,    -- override the file sorter
+            case_mode = 'smart_case',       -- or "ignore_case" or "respect_case"
           },
         },
       })
+      -- To get fzf loaded and working with telescope, you need to call
+      -- load_extension, somewhere after setup function:
+      require('telescope').load_extension('fzf')
     end,
   },
   {
     'nvim-telescope/telescope-file-browser.nvim',
     dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
     keys = {
-      { '<leader>f', desc = 'File' },
+      { '<leader>f',  desc = 'File' },
       { '<leader>fb', mode = { 'n', 'v' }, '<cmd>Telescope file_browser<cr>', desc = 'File Browser' },
     },
     config = function()
