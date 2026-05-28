0e2ad313fbed76bce5eac5fcf9d102f6d1f54f0f
missou <695383649@qq.com>
2026-05-28 20:26:19 +0800

    update blinkcmp

--- lua/plugins/blinkcmp.lua
+++ lua/plugins/blinkcmp.lua
@@ -3,8 +3,16 @@
 local M = {
   'saghen/blink.cmp',
   dependencies = {
+    'saghen/blink.lib',
+    -- optional: provides snippets for the snippet source
+    'rafamadriz/friendly-snippets',
     'Kaiser-Yang/blink-cmp-avante',
   },
+  build = function()
+    -- build the fuzzy matcher, wait up to 60 seconds
+    -- you can use `gb` in `:Lazy` to rebuild the plugin as needed
+    require('blink.cmp').build():wait(60000)
+  end,
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
@@ -13,18 +21,20 @@ local M = {
     -- 'super-tab' for mappings similar to vscode (tab to accept)
     -- 'enter' for enter to accept
     -- 'none' for no mappings
-    keymap = { preset = 'super-tab' },
-
-    appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono'
-    },
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'default' },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = true } },
+    completion = { documentation = { auto_show = false } },
 
-    -- Default list of enabled providers defined so that you can extend it
+    -- (Default) list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       default = { 'avante', 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
@@ -44,16 +54,14 @@ local M = {
         }
       },
     },
+
     snippets = { preset = 'luasnip' },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
+    -- You may use a lua implementation instead by using `implementation = "lua"`
     -- See the fuzzy documentation for more information
     fuzzy = { implementation = "lua" }
   },
-  opts_extend = { "sources.default" }
 }
 
 return M
